9eed9de11bff203aa5bd343a2bd7b9cbbf503844
jahor <jahor@tuta.io>
2021-03-11 17:45:30 +0300

    Add User model

---
+++ KeyRunnerAPI/models/User.cs
@@ -0,0 +1,9 @@
+namespace KeyRunnerAPI
+{
+    public class User
+    {
+        public int Id { get; set; } = 0;
+        public string Name { get; set; } = "Ivan";
+        public string Username { get; set; } = "Ivan";
+    }
+}
