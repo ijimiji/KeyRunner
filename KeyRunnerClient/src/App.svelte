4c079ddec33f7ad6adb461ce71efc69a56fd4fa6
jahor <mrojahor@gmail.com>
2021-03-12 12:46:50 +0300

    Add Svelte

---
+++ KeyRunnerClient/src/App.svelte
@@ -0,0 +1,12 @@
+<script>
+	import { onMount } from 'svelte'
+	let data
+	onMount(async () => {
+		data = await fetch('https://localhost:5001/User/Get').then(x => x.json())
+	})
+</script>
+
+<pre>
+	<h1>Hello world</h1>
+	<div>{JSON.stringify(data)}<div>
+</pre>
